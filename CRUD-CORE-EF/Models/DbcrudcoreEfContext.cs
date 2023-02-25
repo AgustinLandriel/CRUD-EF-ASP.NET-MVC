b63c84cd10bb8081650a968a86404beecea653ac
Agustin Landriel <agusrojo20@hotmail.com>
2023-02-25 00:06:21 -0300

    Creacion del proyecto y configuracion de entity framework core

---
+++ CRUD-CORE-EF/Models/DbcrudcoreEfContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_CORE_EF.Models;
+
+public partial class DbcrudcoreEfContext : DbContext
+{
+    public DbcrudcoreEfContext()
+    {
+    }
+
+    public DbcrudcoreEfContext(DbContextOptions<DbcrudcoreEfContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cargo> Cargos { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=CHUCHO\\SQLEXPRESS; DataBase=DBCRUDCORE_EF;Integrated Security=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cargo>(entity =>
+        {
+            entity.HasKey(e => e.IdCargo).HasName("PK__CARGO__6C98562500645EEC");
+
+            entity.ToTable("CARGO");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__EMPLEADO__CE6D8B9EE2207F9C");
+
+            entity.ToTable("EMPLEADO");
+
+            entity.Property(e => e.Correo)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdCargoNavigation).WithMany(p => p.Empleados)
+                .HasForeignKey(d => d.IdCargo)
+                .HasConstraintName("FK__EMPLEADO__IdCarg__267ABA7A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
